277d7a95121cf172abde26f78b1d0004a3a86f25
Swamita <swamita2001@gmail.com>
2021-03-20 00:59:53 +0530

    feat: add data page

---
+++ Crowd Monitor/DataViewController.swift
@@ -0,0 +1,36 @@
+//
+//  DataViewController.swift
+//  Crowd Monitor
+//
+//  Created by Swamita on 20/03/21.
+//
+
+import UIKit
+
+class DataViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    let players = ["10-12pm","12-2pm", "2-4pm", "4-6pm", "6-8pm", "8-10pm"]
+    let goals = ["6", "15", "26", "30", "8", "10"]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        tableView.dataSource = self
+        tableView.register(UINib(nibName: "DataTableViewCell", bundle: nil), forCellReuseIdentifier: "dataCell")
+
+    }
+}
+
+extension DataViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return players.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "dataCell") as! DataTableViewCell
+        cell.countLabel.text = goals[indexPath.row]
+        cell.timeLabel.text = players[indexPath.row]
+        return cell
+    }
+}
--- Crowd Monitor/LogViewController.swift
+++ Crowd Monitor/LogViewController.swift
@@ -25,6 +25,14 @@ class LogViewController: UIViewController {
         
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        navigationController?.navigationBar.isHidden = true
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        navigationController?.navigationBar.isHidden = false
+    }
+    
     @IBAction func segmentChanged(_ sender: Any) {
         switch segmentedControl.selectedSegmentIndex
             {
@@ -69,17 +77,5 @@ class LogViewController: UIViewController {
         chartDataSet.colors = ChartColorTemplates.material()
         let chartData = BarChartData(dataSet: chartDataSet)
         barChartView.data = chartData
-    }
-    
-    private func colorsOfCharts(numbersOfColor: Int) -> [UIColor] {
-      var colors: [UIColor] = []
-      for _ in 0..<numbersOfColor {
-        let red = Double(arc4random_uniform(256))
-        let green = Double(arc4random_uniform(256))
-        let blue = Double(arc4random_uniform(256))
-        let color = UIColor(red: CGFloat(red/255), green: CGFloat(green/255), blue: CGFloat(blue/255), alpha: 1)
-        colors.append(color)
-      }
-      return colors
     }
 }
