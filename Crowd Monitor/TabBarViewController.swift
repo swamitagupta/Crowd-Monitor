baf78fc6ede4d0a54e3b5cebd56357fa207f0280
Swamita <swamita2001@gmail.com>
2021-03-20 11:49:33 +0530

    feat: complete scheduling page

---
+++ Crowd Monitor/TabBarViewController.swift
@@ -0,0 +1,30 @@
+//
+//  TabBarViewController.swift
+//  Crowd Monitor
+//
+//  Created by Swamita on 20/03/21.
+//
+
+import UIKit
+
+class TabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.selectedIndex = 1
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
